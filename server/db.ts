bbd7d1e043862a61166fa28bb9f56737fa64ca36
Ramon01bvy <140982028+Ramon01bvy@users.noreply.github.com>
2025-09-02 19:37:49 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -5,7 +5,7 @@ import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.postgresql://neondb_owner:npg_MzZhwgE5X8WT@ep-cool-field-af65jghj.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
